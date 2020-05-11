aa39bfef7ad333b9edbf09a8911e424de4160a44
karlave14 <karla_valdeze14@hotmail.com>
2020-05-11 17:48:51 -0500

    Ejercicios con IF

---
+++ conditional.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	if(x>=1 && x<6){
+	 printf("Hello")}
+	else if(x>=6 && x<11)
+	{ printf("world");}
+	if else (x>11 && x<15)
+	{ printf("goku");{
+	else printf("nobody"){
+
+	}
+
+
+}
